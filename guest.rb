4a46ed9b2b5affe11faa03e7b08be34035c7ce15
user <user@users-MacBook-Pro.local>
2018-12-10 10:21:52 +0000

    Guest class and getters created and tested.

---
+++ guest.rb
@@ -0,0 +1,11 @@
+class Guest
+
+  attr_reader :guest_name, :guest_favourite_song, :guest_money
+
+  def initialize(guest_name, guest_favourite_song, guest_money)
+    @guest_name = guest_name
+    @guest_favourite_song = guest_favourite_song
+    @guest_money = guest_money
+  end
+
+end
---
+++ specs/guest_spec.rb
@@ -0,0 +1,23 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../guest')
+
+class GuestTest < MiniTest::Test
+
+  def setup
+    @guest = Guest.new("Jennifer", "Time in a Bottle", 20)
+  end
+
+  def test_guest_name
+    assert_equal("Jennifer", @guest.guest_name)
+  end
+
+  def test_guest_favourite_song
+    assert_equal("Time in a Bottle", @guest.guest_favourite_song)
+  end
+
+  def test_guest_money
+    assert_equal(20, @guest.guest_money)
+  end
+
+end
